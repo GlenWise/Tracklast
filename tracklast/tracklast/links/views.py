767eb344bd47116e88a4eee3a4dc8f11f62a25a3
Glen Wise <gwise@andrew.cmu.edu>
2015-12-01 09:29:06 -0500

    Update Trackless

--- tracklast/tracklast/links/views.py
+++ tracklast/tracklast/links/views.py
@@ -1,5 +1,22 @@
 from django.views.generic import ListView
 from .models import Link, Vote
+from django.views.generic.edit import UpdateView
+from .models import UserProfile
+from .forms import UserProfileForm
+from django.core.urlresolvers import reverse
 
 class LinkListView(ListView):
     model = Link
+
+
+
+class UserProfileEditView(UpdateView):
+    model = UserProfile
+    form_class = UserProfileForm
+    template_name = "edit_profile.html"
+
+    def get_object(self, queryset=None):
+        return UserProfile.objects.get_or_create(user=self.request.user)[0]
+
+    def get_success_url(self):
+        return reverse("profile", kwargs={'slug': self.request.user})
--- tracklast/tracklast/tracklast/urls.py
+++ tracklast/tracklast/tracklast/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from links.views import LinkListView
+from django.contrib.auth.decorators import login_required as auth
+from links.views import UserProfileEditView
 
 # Uncomment the next two lines to enable the admin:
 
@@ -17,5 +19,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', LinkListView.as_view(), name='home'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {
+    'template_name': 'login.html'}, name="login"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login',
+    name="logout"),
+    url(r'^edit_profile/$', auth(UserProfileEditView.as_view()), 
+                                            name="edit_profile"),
 
 )
