8401f99b93e2d7a4f48e418f303840e4c1dd0f11
Glen Wise <gwise@andrew.cmu.edu>
2015-12-02 15:33:23 -0500

    Deleted Unnecessary files

--- tracklastproject/links/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import ListView
-from .models import Link, Vote
-
-class LinkListView(ListView):
-    model = Link
-    queryset = Link.with_votes.all()
-    paginate_by = 1
